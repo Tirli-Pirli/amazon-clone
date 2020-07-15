6a18229bb35da2fde520ca23fe0747f1eace887c
Jurgis <audickasj@gmail.com>
2020-07-15 19:08:58 +0300

    first commit

---
+++ backend/data.js
@@ -0,0 +1,40 @@
+export default  {
+    products: [{
+        _id: '1',
+        name: 'Slim Shirt',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '2',
+        name: 'Jeans',
+        image: '/images/d1.jpg',
+        price: 100,
+        brand: 'Armani',
+        rating: 4.5,
+        numReviews: 12
+    },
+    {
+        _id: '3',
+        name: 'Shorts',
+        image: '/images/d1.jpg',
+        price: 20,
+        brand: 'Bench',
+        rating: 4,
+        numReviews: 120
+    },
+    {
+        _id: '4',
+        name: 'Shorts',
+        image: '/images/d1.jpg',
+        price: 20,
+        brand: 'Bench',
+        rating: 4,
+        numReviews: 120
+    },
+    
+]
+}
