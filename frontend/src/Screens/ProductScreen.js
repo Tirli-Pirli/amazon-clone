54ff0f0ed77ae29598931dc8f2941efd1d18f8a3
Jurgis <audickasj@gmail.com>
2020-07-25 19:17:56 +0300

    checkout page

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -2,21 +2,29 @@ import React, { useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { detailsProduct } from '../actions/productActions';
+import { useState } from 'react';
 
 function ProductScreen(props){
 
+    const [qty, setQty] = useState(1)
     const productDetails = useSelector(state => state.productDetails);
-    const dispatch = useDispatch;
+    const {product, loading, error} = productDetails;
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(detailsProduct())
+        dispatch(detailsProduct(props.match.params.id))
     }, []);
+
+    const handleAddToCart = () => {
+        props.history.push('/cart/' + props.match.params.id + '?qty=' + qty)
+    }
 
     return (
     <div className="back-to-result">
         <div>
             <Link to="/">Back to result</Link>
         </div>
+    { loading?  <div>Loading...</div> : error? <div>{error}</div> : (
         <div className="details">
             <div className="details-image">
                 <img src={product.image} alt="product"/>
@@ -46,22 +54,23 @@ function ProductScreen(props){
                         Price: {product.price}
                     </li>
                     <li>
-                        Status: {product.status}
+                        Status: {product.countInStock > 0 ? 'In Stock' : 'Unavailable'}
                     </li>
                     <li>
-                        Qty: <select>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
+                        Qty: <select value={qty} onChange={(e) => {setQty(e.target.value)}}>
+                            {[...Array(product.countInStock).keys()].map(x => 
+                                <option key={x+1} value={x+1}>{x+1}</option>
+                                )}
                         </select>
                     </li>
                     <li>
-                        <button className="button">Add to Cart</button>
+                        {product.countInStock > 0 && 
+                        <button onClick={handleAddToCart} className="button">Add to Cart</button>}
                     </li>
                 </ul>
             </div>
         </div>
+        )}
     </div>
     )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './Reducers/productReducers';
-
+import { productListReducer, productDetailsReducer } from './Reducers/productReducers';
+import {cartReducer} from './Reducers/cartReducers'
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails : productDetailsReducer,
+    cart: cartReducer
 })
 
 const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
